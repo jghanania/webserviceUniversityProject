f9c448e22021c5de4b1bc97141e9b2153bc27a5f
jghanania <jean-gabriel.hanania@stud.fra-uas.de>
2024-12-21 19:08:51 +0100

    Spring Boot Project for the expenses tracker including GraphQL dependency

---
+++ ExpensesTrackerService/src/main/java/edu/fra/uas/ExpensesTrackerService/ExpensesTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.ExpensesTrackerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ ExpensesTrackerService/src/test/java/edu/fra/uas/ExpensesTrackerService/ExpensesTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.ExpensesTrackerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
