6a9925c0d449325b3eaf5a3bef5604684d38603d
Meeraf1 <meerafgolja@gmail.com>
2025-01-30 16:54:25 +0100

    Update Userservice

--- UserService/userservice/src/main/java/edu/fra/uas/userservice/UserserviceApplication.java
+++ UserService/userservice/src/main/java/edu/fra/uas/userservice/UserserviceApplication.java
@@ -1,13 +1,30 @@
 package edu.fra.uas.userservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import edu.fra.uas.userservice.service.UserService;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserserviceApplication.class, args);
+    }
 
+    // CommandLineRunner für initiale Benutzerdaten
+    @Bean
+    public CommandLineRunner initData(UserService userService) {
+        return args -> {
+            // Beispielbenutzer hinzufügen
+            userService.createUser("Jean", "Jean@example.com");
+            userService.createUser("Emre", "Emre@example.com");
+            userService.createUser("Daniel", "Daniel@example.com");
+            userService.createUser("Lehmann", "Lehmann@example.com");
+
+            System.out.println("Initial users created in the database!");
+        };
+    }
 }
