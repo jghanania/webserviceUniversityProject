6352935f0ddf63d08c40304a1b66837e054d7203
Meeraf1 <meerafgolja@gmail.com>
2025-02-03 10:11:15 +0100

    Status Codes eingebaut

--- UserService/userservice/src/main/java/edu/fra/uas/userservice/service/UserService.java
+++ UserService/userservice/src/main/java/edu/fra/uas/userservice/service/UserService.java
@@ -2,7 +2,9 @@ package edu.fra.uas.userservice.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import edu.fra.uas.userservice.model.User;
 import edu.fra.uas.userservice.model.UserRepository;
@@ -12,35 +14,58 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-// Konstruktor 
+    /**
+     * Constructor for UserService.
+     *
+     * @param userRepository Repository for user data.
+     */
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    // Benutzer erstellen
+    /**
+     * Creates a new user.
+     *
+     * @param name  The name of the user.
+     * @param email The email of the user.
+     * @return The created user.
+     */
     public User createUser(String name, String email) {
         User user = new User(name, email);
         return userRepository.save(user);
     }
 
-    // Benutzer nach ID abrufen
+    /**
+     * Retrieves a user by their ID.
+     *
+     * @param id The ID of the user.
+     * @return The user with the matching ID.
+     * @throws ResponseStatusException if the user is not found (404 NOT FOUND).
+     */
     public User getUser(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Benutzer mit ID " + id + " nicht gefunden"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User with ID " + id + " not found"));
     }
 
-    // Liste aller Benutzer abrufen
+    /**
+     * Retrieves a list of all users.
+     *
+     * @return A list of all users.
+     */
     public List<User> listUsers() {
         return userRepository.findAll();
     }
 
-    // Löschen User Id
-    public void deletUser(Long id) {
-        if (userRepository.existsById(id)) {
-            userRepository.deleteById(id);
-        } else {
-            throw new IllegalArgumentException("User mit der ID" + id + "not found.");
+    /**
+     * Deletes a user by their ID.
+     *
+     * @param id The ID of the user to delete.
+     * @throws ResponseStatusException if the user is not found (404 NOT FOUND).
+     */
+    public void deleteUser(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User with ID " + id + " not found");
         }
+        userRepository.deleteById(id);
     }
-
 }
